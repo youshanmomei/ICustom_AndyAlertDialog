4d2b07b3a7a18b2b02f239781df67ccb69074c45
qc <1142041743@qq.com>
2016-08-14 23:43:54 +0800

    1.AndyAlertController(); 2.ButtonHandler();

--- app/src/main/java/org/qc/icustom_andyalertdialog/util/dialog/AndyAlertController.java
+++ app/src/main/java/org/qc/icustom_andyalertdialog/util/dialog/AndyAlertController.java
@@ -1,9 +1,15 @@
 package org.qc.icustom_andyalertdialog.util.dialog;
 
 import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Handler;
+import android.os.Message;
+import android.view.Window;
 import android.widget.LinearLayout;
 
 import org.qc.icustom_andyalertdialog.R;
+
+import java.lang.ref.WeakReference;
 
 /**
  * Created by admin on 2016/8/9.
@@ -15,8 +21,38 @@ public class AndyAlertController {
 
 
     public static int m_MyAlertContentViewId = R.layout.andy_alertex_dialog_layout;
+    private final DialogInterface mDialogInterface;
+    private final Window mWindow;
+    private final ButtonHandler mHandler;
 
-    public AndyAlertController(Context mContext) {
+
+    private class ButtonHandler extends Handler {
+        private static final int MSG_DISMISS_DIALOG = 1;
+        private WeakReference<DialogInterface> mDialog;
+
+        public ButtonHandler(DialogInterface dialog) {
+            WeakReference<DialogInterface> mDialog = new WeakReference<DialogInterface>(dialog);
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case DialogInterface.BUTTON_POSITIVE:
+                case DialogInterface.BUTTON_NEGATIVE:
+                case DialogInterface.BUTTON_NEUTRAL:
+                    ((DialogInterface.OnClickListener) msg.obj).onClick(mDialog.get(), msg.what);
+                    break;
+                case MSG_DISMISS_DIALOG:
+                    ((DialogInterface)msg.obj).dismiss();
+            }
+        }
+    }
+
+    public AndyAlertController(Context mContext, DialogInterface di, Window window) {
         this.mContext = mContext;
+        mDialogInterface = di;
+        mWindow = window;
+        mHandler = new ButtonHandler(di);
+
     }
 }
