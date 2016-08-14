b47196664f56f1f11a3a844219eae1b12e03b552
qc <1142041743@qq.com>
2016-08-15 06:15:02 +0800

    1.canTextInput(); 2.installContent();

--- app/src/main/java/org/qc/icustom_andyalertdialog/util/dialog/AndyAlertController.java
+++ app/src/main/java/org/qc/icustom_andyalertdialog/util/dialog/AndyAlertController.java
@@ -4,7 +4,11 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Handler;
 import android.os.Message;
+import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.LinearLayout;
 
 import org.qc.icustom_andyalertdialog.R;
@@ -23,10 +27,39 @@ public class AndyAlertController {
     public static int m_MyAlertContentViewId = R.layout.andy_alertex_dialog_layout;
     private final DialogInterface mDialogInterface;
     private final Window mWindow;
-    private final ButtonHandler mHandler;
+    private View mView;
 
+    private final ButtonHandler mHandler;
+    private Button mButonPositive;
+    private Message mButtonPositiveMessage;
+    private Button mButtonNegative;
+    private Message mButtonNegativeMessage;
+    private Button mButtonNeutral;
+    private Message mButtonNeutralMessage;
+    View.OnClickListener mButtonHandler = new View.OnClickListener(){
+
+        @Override
+        public void onClick(View view) {
+            Message m = null;
+            if (view == mButonPositive && mButtonPositiveMessage!=null){
+                m = Message.obtain(mButtonPositiveMessage);
+            } else if (view == mButtonNegative && mButtonNegativeMessage != null) {
+                m = Message.obtain(mButtonNegativeMessage);
+            } else if (view == mButtonNeutral && mButtonNeutralMessage !=null) {
+                m = Message.obtain(mButtonNeutralMessage);
+            }
+
+            if (m == null) {
+                m.sendToTarget();
+            }
+
+            //post a message so we dismiss after the above handlers are executed
+            mHandler.obtainMessage(ButtonHandler.MSG_DISMISS_DIALOG, mDialogInterface).sendToTarget();
+        }
+    };
 
     private class ButtonHandler extends Handler {
+        //Button clicks have Message.what as the BUTTON{1,2,3} constant
         private static final int MSG_DISMISS_DIALOG = 1;
         private WeakReference<DialogInterface> mDialog;
 
@@ -55,4 +88,41 @@ public class AndyAlertController {
         mHandler = new ButtonHandler(di);
 
     }
+
+    static boolean canTextInput(View v) {
+        if (v.onCheckIsTextEditor()) {
+            return true;
+        }
+
+        if (!(v instanceof ViewGroup)) {
+            return false;
+        }
+
+        ViewGroup vg = (ViewGroup) v;
+        int i = vg.getChildCount();
+        while (i > 0) {
+            i--;
+            v = vg.getChildAt(i);
+            if (canTextInput(v)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void installContent() {
+        //we use a custom title so never request a window title
+        mWindow.requestFeature(Window.FEATURE_NO_TITLE);
+
+        if (mView == null && !canTextInput(mView)) {
+            mWindow.setFlags(WindowManager.LayoutParams.FLAG_ALT_FOCUSABLE_IM, WindowManager.LayoutParams.FLAG_ALT_FOCUSABLE_IM);
+        }
+        mWindow.setContentView(m_MyAlertContentViewId);
+        setupView();
+    }
+
+    private void setupView() {
+
+    }
+
 }
